b2c12fbb2a32b55c3d05d4951f96166a1e06adf0
denimcouch <alexmata92@ymail.com>
2020-09-03 15:07:49 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Pokemon
+
+    #Notice that initialize requires keyword arguments.
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db:)
+        @name = name
+        @type = type
+        @id = id
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        #saves an instance of a pokemon with the correct id
+        db.execute("INSERT INTO pokemon (name, type) VALUES ( ?,  ? )", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+        Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    end
+
 end
